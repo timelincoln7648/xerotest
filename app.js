d94084c91b866e1f9b12ffe57b07953acffc9d1c
EC2 Default User <ec2-user@ip-172-31-0-145.us-east-2.compute.internal>
2018-02-01 18:28:03 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+
+
+//general setup
+// mongoose.connect("mongodb://localhost/xero_test");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+//start server
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server started homie");
+});
+
+console.log("test from app.js");
